677d2feff8a6be6a20e1908025fbc0fdb9d2e670
FarukC5 <faruk_cajic@yahoo.com>
2021-04-02 03:03:32 +0200

    Fetch - GET with headers

---
+++ ex04/src/GetRequestSetHeaders.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class GetRequestSetHeaders extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { totalReactpackages: null };
+  }
+
+  componentDidMount() {
+    const headers = { "Content-Type": "application/json" };
+    fetch("https://api.npms.io/v2/search?q=react", { headers })
+      .then((response) => response.json())
+      .then((data) => this.setState({ totalReactpackages: data.total }));
+  }
+
+  render() {
+    const { totalReactpackages } = this.state;
+    return (
+      <div className="card text-center m-3">
+        <h5 className="card-header">GET Request with Set Headers</h5>
+        <div className="card-body">Total packages: {totalReactpackages}</div>
+      </div>
+    );
+  }
+}
+
+export default GetRequestSetHeaders;
